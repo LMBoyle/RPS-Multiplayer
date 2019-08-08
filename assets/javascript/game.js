81a29bb57e9f93465a6dcb07ace189aaf00cacaa
LMBoyle <boylelukem@gmail.com>
2019-08-08 12:22:06 -0400

    pseduocoded javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// VARS ======================================
+// Function(s) ===============================
+// TODO User types in name
+// TODO Show game waiting screen
+// TODO Second user types in name
+// TODO Show selection screen
+// TODO User 1 makes selection
+// TODO User 2 makes selection
+// TODO Push selections to firebase
+// TODO Show each users selection
+// TODO Show which user won
+// TODO Update win and loss
+// TODO Show chat
+// TODO If user types something, push to firebase 
+// TODO Show chat message on both screens
+
+// Call ======================================
+
